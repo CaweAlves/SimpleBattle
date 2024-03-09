14ac77801132e5d484b7236f69d2bb6ba143b4c3
Cawe Alves <cawe.wewe04@gmail.com>
2024-03-09 13:04:21 -0300

    init

---
+++ src/main/java/com/cawe/SimpleBattle/SimpleBattleApplication.java
@@ -0,0 +1,13 @@
+package com.cawe.SimpleBattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBattleApplication.class, args);
+	}
+
+}
